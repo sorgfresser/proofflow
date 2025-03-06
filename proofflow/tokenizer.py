ce0c215b52303799a5290397a208cc61bf25b811
Simon Sorg <simon.sorg@student.hpi.de>
2025-03-06 21:30:19 +0000

    Add tokenizer based on mathlib

---
+++ proofflow/tokenizer.py
@@ -0,0 +1,24 @@
+"""
+Code to create a BPE tokenizer for Lean 4 on the mathlib data used in lean dojo.
+"""
+
+from tokenizers.trainers import BpeTrainer
+from tokenizers import Tokenizer, Regex
+from tokenizers.models import BPE
+from tokenizers.pre_tokenizers import PreTokenizer, Whitespace, Punctuation, Sequence
+from pathlib import Path
+
+
+tokenizer = Tokenizer(BPE(unk_token="[UNK]"))
+trainer = BpeTrainer(special_tokens=["[UNK]", "[PAD]", "[EOS]", "[PROOFSTATE]", "[PROOFSTEP]", "[TACTICS]"])
+
+# We split on whitespaces or periods, as periods are used frequently in imports
+# For example, we do not want multiple namespaces in one token
+# tokenizer.pre_tokenizer = Split(Regex(r"\w+|[^\w\s]+|\."), behavior="isolated")
+# tokenizer.pre_tokenizer = Whitespace()
+tokenizer.pre_tokenizer = Sequence([Punctuation(), Whitespace()])
+mathlib_path = Path("../mathlib4")
+files = [str(file.resolve()) for file in mathlib_path.glob("**/*.lean")]
+
+tokenizer.train(files, trainer)
+tokenizer.save("lean_tokenizer.json")
