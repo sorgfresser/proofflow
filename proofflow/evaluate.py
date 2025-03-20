8216573084fa88a9819250626b3784969c1c2817
tss52 <tss52@cam.ac.uk>
2025-03-20 01:06:01 +0000

    Add reprover boolean

--- proofflow/evaluate.py
+++ proofflow/evaluate.py
@@ -2,7 +2,7 @@ import tempfile
 
 from transformers import PreTrainedTokenizerFast
 import torch
-from proofflow.policy import MambaPolicy
+from proofflow.policy import MambaPolicy, ReProverPolicy
 from proofflow.data import ProofStateDataset
 from proofflow.train_gfn import MCTS, Node, sample_mcts_trajectories, collate_skip_none, evaluate
 from pathlib import Path
@@ -23,6 +23,7 @@ def main():
     parser.add_argument("--num-tactics", type=int, default=32)
     parser.add_argument("--search-time", type=int, default=10)
     parser.add_argument("--num-workers", type=int, default=4)
+    parser.add_argument("--reprover", action="store_true", default=False)
     args = parser.parse_args()
     checkpoint_path = Path(args.checkpoint_path)
     json_path = Path(args.json_path)
@@ -30,10 +31,12 @@ def main():
     batch_size = args.batch_size
     num_tactics = args.num_tactics
     num_workers = args.num_workers
+    reprover = args.reprover
 
     assert checkpoint_path.exists() and json_path.exists()
     config = {"num_workers": num_workers, "batch_size": batch_size, "num_tactics": num_tactics,
-              "search_time": search_time, "checkpoint_path": checkpoint_path, "json_path": json_path}
+              "search_time": search_time, "checkpoint_path": checkpoint_path, "json_path": json_path,
+              "reprover": reprover}
     wandb.init(project="proofflow", config=config)
 
     tokenizer = PreTrainedTokenizerFast.from_pretrained("./lean_tokenizer")
@@ -42,7 +45,10 @@ def main():
     with TemporaryDirectory() as tmpdir:
         handler_fac = lambda: LeanREPLHandler(Path("./leanproject"))
         ds = ProofStateDataset(json_path, handler_fac, Path("./mathlib4"), Path(tmpdir), filter_lake=False)
-        policy = MambaPolicy.from_file(checkpoint_path, True, tokenizer, device)
+        if reprover:
+            policy = ReProverPolicy.from_pretrained("cuda", False)
+        else:
+            policy = MambaPolicy.from_file(checkpoint_path, True, tokenizer, device)
         policy.model.eval()
         loader = DataLoader(ds, batch_size=batch_size, shuffle=False, collate_fn=collate_skip_none,
                             num_workers=num_workers)
